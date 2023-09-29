3fa90c0e13125a4f9faceb87b2c24001181049f7
Rohit.D <dhumakrohit9@gmail.com>
2023-09-29 23:57:29 +0530

    this is second file

---
+++ 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    cout<<"Enter the year = ";
+    cin>>year;
+
+
+    if(year % 4 == 0)
+    {
+        cout << year <<"is a leap year ";
+
+    }
+    else 
+    {
+        cout<< year <<" is not leap year";
+
+    }
+
+    return 0 ;
+
+}

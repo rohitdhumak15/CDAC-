04273984a9a1aa0f00518e0edd758a841886bfce
Rohit.D <dhumakrohit9@gmail.com>
2023-09-30 17:23:44 +0530

    ADD THE NEW FILE

---
+++ class2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num1;
+    int num2;
+    int num3;
+
+    cout<<"Enter the num1 and num2"<<endl;
+    cin>>num1>>num2;
+
+    num3 = num1*num2;
+
+    cout<<"num3 = "<<num3;
+
+    return 0;
+
+}
